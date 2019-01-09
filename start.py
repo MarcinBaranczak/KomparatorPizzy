03de88eba7ad735ecd9dee46919e4d4e3f776e51
Marcin Barańczak <46418050+MarcinBaranczak@users.noreply.github.com>
2019-01-09 08:28:01 +0100

    ver 0.1

---
+++ start.py
@@ -0,0 +1,34 @@
+import math
+
+# ----- pobiera ilość wykonań -----
+def start():
+    ilosc = int(input("Podaj ilość pizz do porównania: "))
+
+    tabPola = []
+    for i in range(ilosc):
+        tabPola.append(pobierzDane())
+    # endFor
+
+    for i in tabPola:
+        obliczPizze(i)
+    # endFor
+
+
+# ----- oblicza i wyświetla dane -----
+def obliczPizze(r):
+    print("pole: ", obliczPole(r))
+
+
+# ----- pobiera dane pizzy -----
+def pobierzDane():
+    return float(input("podaj wielkość pizzy: ")) / 2
+
+
+# ----- oblicza pole koła PI*r^2
+def obliczPole(r):
+    return int(math.pi * (r ** 2))
+
+
+# ----- program główny -----
+
+start()
